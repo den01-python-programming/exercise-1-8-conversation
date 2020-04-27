2eb0378b496b4a3d87d0c411bb504dd0ea84f6f1
Scott Morgan <smorgan@bridgend.ac.uk>
2020-04-27 20:04:47 +0100

    Updates testing routine

--- src/exercise.py
+++ src/exercise.py
@@ -1,10 +1,5 @@
 def main():
   #write your code below this line
-  message = input("Greetings! How are you doing?")
-  print(message)
-  message = input("Oh, how interesting. Tell me more!")
-  print(message)
-  print("Thanks for sharing!")
 
 if __name__ == '__main__':
     main()
