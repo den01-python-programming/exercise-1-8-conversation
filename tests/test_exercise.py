8a2e8c179ac0f62c699b6db13524936abedb57a3
Scott Morgan <smorgan@bridgend.ac.uk>
2020-05-11 17:58:46 +0100

    Updates testing routine

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -3,7 +3,6 @@ import src.exercise
 
 def test_exercise():
     input_values = ["Good thank you!","Well, there's really nothing to tell."]
-    input_values_stored = ["Good thank you!","Well, there's really nothing to tell."]
     output = []
 
     def mock_input(s=None):
@@ -19,5 +18,4 @@ def test_exercise():
 
     src.exercise.main()
 
-    assert [output[0],output[2],output[4]] == ["Greetings! How are you doing?","Oh, how interesting. Tell me more!","Thanks for sharing!"]
-    assert [output[1],output[3]] == [input_values_stored[0],input_values_stored[1]]
+    assert output == ["Greetings! How are you doing?","Oh, how interesting. Tell me more!","Thanks for sharing!"]
